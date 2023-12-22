4413782a47774981a1d5d6f04bc00177ce590eed
takakiono <takaki.ono@icloud.com>
2023-12-22 14:01:02 +0900

    画面で家計簿情報取得APIを呼び出す処理を追加

---
+++ frontend/src/GetAccount.tsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import "./common.css";
+
+interface Account {
+    accountId: string;
+    groupId: string;
+    categoryId: string;
+    note: string;
+    amount: string;
+    datetime: string;
+}
+  
+interface ApiResponse {
+  accounts: Account[];
+}
+
+export const GetAccount = () =>{
+    const [accounts, setAccounts] = useState<Account[]>([]);
+  
+    useEffect(() => {
+      async function fetchData() {
+        try {
+          const response = await fetch("http://0.0.0.0:8801/nacha/account?groupId=1&acquisitionMonth=2023-11", { method: "GET" });
+          const data: ApiResponse = await response.json();
+          setAccounts(data.accounts);
+        } catch (error) {
+          console.error('データの取得に失敗しました', error);
+        }
+      }
+  
+      fetchData();
+    }, []);
+  
+    return (
+      <div>
+        <h1>Account Table</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>家計簿ID</th>
+              <th>グループID</th>
+              <th>カテゴリID</th>
+              <th>備考</th>
+              <th>金額</th>
+              <th>日付</th>
+            </tr>
+          </thead>
+          <tbody>
+            {accounts.map(account => (
+              <tr key={account.accountId}>
+                <td>{account.accountId}</td>
+                <td>{account.groupId}</td>
+                <td>{account.categoryId}</td>
+                <td>{account.note}</td>
+                <td>{account.amount}円</td>
+                <td>{account.datetime}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+}
--- frontend/src/GetUser.tsx
+++ frontend/src/GetUser.tsx
@@ -15,7 +15,7 @@ export const GetUser = () =>{
     useEffect(() => {
       async function fetchData() {
         try {
-          const response = await fetch("http://0.0.0.0:8801/nacha/user?groupId=1", { method: "GET" });
+          const response = await fetch("http://0.0.0.0:8801/nacha/group/user?groupId=1", { method: "GET" });
           const data: ApiResponse = await response.json();
           setUsers(data.users);
         } catch (error) {
@@ -29,14 +29,22 @@ export const GetUser = () =>{
     return (
       <div>
         <h1>User List</h1>
-        <ul>
-          {users.map(user => (
-            <li key={user.userId}>
-                {user.userId}:
-                {user.userName}
-            </li>
-          ))}
-        </ul>
+        <table>
+          <thead>
+            <tr>
+              <th>ユーザID</th>
+              <th>名前</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(user => (
+              <tr key={user.userId}>
+                  <td>{user.userId}</td>
+                  <td>{user.userName}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
 }
